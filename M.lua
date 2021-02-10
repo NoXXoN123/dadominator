53cf4132da86e75d958a0d9ba4cf6a3c2fecdeb1
NoXXoN123 <63915410+NoXXoN123@users.noreply.github.com>
2021-02-10 10:35:59 +0100

    Create M.lua

---
+++ M.lua
@@ -0,0 +1,16 @@
+{
+  label = "M",
+  shapes = {
+    { 2,0,0,3,1,16,texture = "iron_block", tint = 0x111111 },
+    { 3,0,14,4,1,15,texture = "iron_block", tint = 0x111111 },
+    { 4,0,13,5,1,14,texture = "iron_block", tint = 0x111111 },
+    { 5,0,12,6,1,13,texture = "iron_block", tint = 0x111111 },
+    { 6,0,11,7,1,12,texture = "iron_block", tint = 0x111111 },
+    { 7,0,10,9,1,11,texture = "iron_block", tint = 0x111111 },
+    { 9,0,11,10,1,12,texture = "iron_block", tint = 0x111111 },
+    { 10,0,12,11,1,13,texture = "iron_block", tint = 0x111111 },
+    { 11,0,13,12,1,14,texture = "iron_block", tint = 0x111111 },
+    { 12,0,14,14,1,15,texture = "iron_block", tint = 0x111111 },
+    { 13,0,0,14,1,16,texture = "iron_block", tint = 0x111111 },
+  }
+}
