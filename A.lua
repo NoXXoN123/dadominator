48d8a8859163abd0df3034916509e4a0e0a7252a
NoXXoN123 <63915410+NoXXoN123@users.noreply.github.com>
2021-02-10 10:33:08 +0100

    Create A.lua

---
+++ A.lua
@@ -0,0 +1,17 @@
+{
+  label = "A",
+  shapes = {
+    { 2,0,0,3,1,4,texture = "iron_block", tint = 0x111111 },
+    { 3,0,3,4,1,7,texture = "iron_block", tint = 0x111111 },
+    { 4,0,6,5,1,10,texture = "iron_block", tint = 0x111111 },
+    { 5,0,8,12,1,9,texture = "iron_block", tint = 0x111111 },
+    { 5,0,9,6,1,13,texture = "iron_block", tint = 0x111111 },
+    { 6,0,13,7,1,15,texture = "iron_block", tint = 0x111111 },
+    { 7,0,15,9,1,16,texture = "iron_block", tint = 0x111111 },
+    { 9,0,13,10,1,15,texture = "iron_block", tint = 0x111111 },
+    { 10,0,9,11,1,13,texture = "iron_block", tint = 0x111111 },
+    { 11,0,6,12,1,10,texture = "iron_block", tint = 0x111111 },
+    { 12,0,3,13,1,7,texture = "iron_block", tint = 0x111111 },
+    { 13,0,0,14,1,4,texture = "iron_block", tint = 0x111111 },
+  }
+}
