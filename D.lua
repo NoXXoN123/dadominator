71ba05ee76e8a356af549de0c3e26149c6d24a4a
NoXXoN123 <63915410+NoXXoN123@users.noreply.github.com>
2021-02-10 10:34:56 +0100

    Create D.lua

---
+++ D.lua
@@ -0,0 +1,14 @@
+{
+  label = "D",
+  shapes = {
+    { 2,0,0,3,1,16,texture = "iron_block", tint = 0x111111 },
+    { 3,0,0,7,1,1,texture = "iron_block", tint = 0x111111 },
+    { 3,0,15,7,1,16,texture = "iron_block", tint = 0x111111 },
+    { 6,0,1,8,1,2,texture = "iron_block", tint = 0x111111 },
+    { 6,0,14,7,1,16,texture = "iron_block", tint = 0x111111 },
+    { 7,0,2,9,1,3,texture = "iron_block", tint = 0x111111 },
+    { 7,0,13,8,1,15,texture = "iron_block", tint = 0x111111 },
+    { 8,0,13,9,1,14,texture = "iron_block", tint = 0x111111 },
+    { 9,0,3,10,1,13,texture = "iron_block", tint = 0x111111 },
+  }
+}
